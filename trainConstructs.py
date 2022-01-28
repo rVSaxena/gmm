a0f1c8b312bb0a2c554382a4fdcef58148502e16
Racing Panda <44290569+rVSaxena@users.noreply.github.com>
2022-01-29 01:32:46 +0530

    renamed train_constructs

---
+++ trainConstructs.py
@@ -0,0 +1,14 @@
+import torch
+import torchaudio
+
+train_constructs={}
+train_constructs["num_components"]=1000
+train_constructs["dimensionality"]=1
+train_constructs["num_iters"]=1000
+train_constructs["logging_dir"]="D:\\Vaibhav_Personal\\Entertainment\\song_generation\\run1_RB\\"
+train_constructs["logging_level"]=20
+
+
+def get_dataset():
+	logging.debug("Loading RB ABUDHABI dataset")
+	return (torchaudio.load("data/RB_ABUDHABI.wav")[0]).numpy().reshape((-1, 1))

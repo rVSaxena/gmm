updates for exp overflow fixes, better logging, added sampling

gmm.py
@@ -1,17 +1,37 @@
 import numpy as np
+from numpy.random import default_rng
 
 class GMM:
 
-	def __init__(self, num_components, dimensionality):
+	def __init__(self, num_components, dimensionality, **kwargs):
+
+		"""
+		kwargs: can supply the regularizer for the determinant of the covariance matrix
+		"""
 
 		self.num_components=num_components
-
+		self.dimensionality=dimensionality
 		mixing_coeffs=np.random.uniform(0, 1, size=(num_components, ))
 		self.mixing_coeffs=mixing_coeffs/np.sum(mixing_coeffs)
 		self.means=np.random.uniform(0, 1, size=(num_components, dimensionality))
 		self.covariances=np.array([np.eye(dimensionality) for _ in range(num_components)])
+		self.reg_covariance_det=kwargs.get("reg_covariance_det", 1e-6)
+		self.rng=default_rng()
 
 		return
+
+
+	def sample(self, n_samples=1):
+
+		sampled_component=(self.rng.multinomial(1, self.mixing_coeffs, size=(n_samples, ))@np.linspace(0, self.num_components-1, self.num_components)).astype('int')
+		raw_samples=self.rng.standard_normal(size=(n_samples, self.dimensionality))
+		sampled_covariances=self.covariances[sampled_component]
+		sampled_means=self.means[sampled_component]
+		cholesky_sampled_covariances=np.linalg.cholesky(sampled_covariances)
+		real_samples=(cholesky_sampled_covariances@raw_samples[:, :, None]).squeeze()+sampled_means
+
+		return real_samples
+
 
 	def pdf(self, x):
 
@@ -21,14 +41,21 @@ class GMM:
 		part_prob=np.zeros((x.shape[0], self.num_components))
 
 		for i in range(self.num_components):
-			part_prob[:, i]=self.mixing_coeffs[i]*np.sqrt((2*np.pi)**(-1*dimensionality))*np.product(
-				np.exp(
-					np.multiply(
-						-0.5*(x-self.means[i]), (x-self.means[i])@np.linalg.inv(self.covariances[i])
-					),
-				),
-			axis=1
-			)/np.sqrt(np.linalg.det(self.covariances[i]))
+			# self.covariances[i]=np.maximum(self.covariances[i], np.eye(self.dimensionality))
+
+			self.covariances[i]=self.covariances[i]+self.reg_covariance_det*np.eye(self.dimensionality)
+
+			exponent=np.multiply(-0.5*(x-self.means[i]), (x-self.means[i])@np.linalg.inv(self.covariances[i]))
+			exponent=exponent.sum(axis=1)
+			# print("max value of exponent of component {} before normalize: {}".format(i, exponent.sum(axis=1).max()))
+			# stability_threshold=exponent.max()-10
+			# print("stability_threshold for {} component: {}".format(i, stability_threshold))
+			# exponent=exponent-stability_threshold
+			# print(exponent)
+
+			exponential=np.exp(exponent)
+			part_prob[:, i]=self.mixing_coeffs[i]*np.sqrt((2*np.pi)**(-1*dimensionality))*exponential
+			part_prob[:, i]=part_prob[:, i]/np.sqrt(np.linalg.det(self.covariances[i]))
 
 		return np.sum(part_prob, axis=1), part_prob
 
@@ -36,6 +63,6 @@ class GMM:
 	def responsibilities(self, x):
 
 		a, b=self.pdf(x)
-		return b/a
+		return b/a[:, None]
 
 	

optimizer.py
@@ -1,54 +1,126 @@
 import logging
-from os.path import join as pathjoin
 import json
-import numpy
+import numpy as np
 from tqdm import tqdm
 from os import makedirs
-from gmm import GMM
-from utils import save_gmm, load_gmm
-from trainConstructs import getDataset, train_constructs
+from os.path import join as pathjoin
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 from matplotlib import pyplot as plt
+from gmm import GMM
+from utils import save_gmm, load_gmm, DataWrapper
+from trainConstructs import getDataset, train_constructs
+import traceback
+import warnings
+import sys
+
+def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
+
+    log = file if hasattr(file,'write') else sys.stderr
+    traceback.print_stack(file=log)
+    log.write(warnings.formatwarning(message, category, filename, lineno, line))
+
+warnings.showwarning = warn_with_traceback
 
 makedirs(train_constructs["logging_dir"], exist_ok=True)
-logging.basicConfig(filename=pathjoin(train_constructs["logging_dir"], ".log"), level=train_constructs["logging_level"])
+logging.basicConfig(filename=pathjoin(train_constructs["logging_dir"], ".log"), level=train_constructs["logging_level"], filemode='w+')
+
+def print_matrix(arr):
+    print(arr.shape)
+    print()
+    for i in range(arr.shape[0]):
+        print()
+        for j in range(arr.shape[1]):
+            print(arr[i, j], end=",")
+    print()
 
 def save_nll_plot():
-	
-	fig, ax=plt.subplots()
-	ax.plot(nlls)
-	fig.suptitle("The negative log likelihood at every iteration")
-	fig.savefig(pathjoin(train_constructs["logging_dir"], "NLL.jpeg"))
+    
+    fig, ax=plt.subplots()
+    ax.plot(nlls)
+    fig.suptitle("The negative log likelihood at every iteration")
+    fig.savefig(pathjoin(train_constructs["logging_dir"], "NLL.jpeg"))
 
-	return
-
-gmm=GMM(
-	train_constructs["num_components"],
-	train_constructs["dimensionality"]
-	)
+    return
 
 dataset=getDataset() # the dataset as a np array
+
+pca=PCA(n_components=30)
+pca.fit(dataset)
+mod_dataset=pca.transform(dataset)
+
+# data_wrapper=DataWrapper(dataset)
+# encoded_dataset=data_wrapper.encode(dataset)
+
+gmm=GMM(
+    train_constructs["num_components"],
+    mod_dataset.shape[1],
+    reg_covariance_det=train_constructs["reg_covar"]
+    )
+
+# Will do KMeans first to find good initialization for means, covariances & mixing coeffs
+
+logging.debug("Starting KMEANS")
+
+km=KMeans(
+  n_clusters=train_constructs["num_components"],
+  max_iter=200
+  )
+
+km.fit(mod_dataset)
+
+y=km.predict(mod_dataset)
+
+for i in range(train_constructs["num_components"]):
+    gmm.means[i]=np.mean(mod_dataset[y==i], axis=0)
+    gmm.covariances[i]=np.cov(mod_dataset[y==i].T) # +np.diag(np.zeros((train_constructs["dimensionality"]))+train_constructs["reg_covar"])
+    logging.debug("Det of cov of {} component: {}".format(i, np.linalg.det(gmm.covariances[i])))
+    gmm.mixing_coeffs[i]=np.sum(y==i)/len(y)
+
+logging.debug("Ending KMEANS")
+
 nlls=[]
 
 logging.debug("Starting EM")
 
 for opt_idx in tqdm(range(train_constructs["num_iters"])):
 
-	# E step
+    logging.debug("Starting iteration {}".format(opt_idx))
 
-	prob, part_prob=gmm.pdf(dataset)
-	responsibilities=part_prob/prob
+    # E step
 
-	# M step
+    prob, part_prob=gmm.pdf(mod_dataset)
+    
+    responsibilities=part_prob/prob[:, None]
 
-	for i in range(gmm.num_components):
-		gmm.means[i]=np.average(dataset, axis=0, weights=responsibilities[:, i])
-		gmm.covariances[i]=np.cov(dataset.T, aweights=responsibilities[:, i])
-		gmm.mixing_coeffs[i]=sum(responsibilities[:, i])/dataset.shape[0]
+    # M step
 
-	nlls.append(-np.sum(np.log(prob)))
+    for i in range(gmm.num_components):
+        logging.debug("M step for {} component".format(i))
+        gmm.means[i]=np.average(mod_dataset, axis=0, weights=responsibilities[:, i])
+        gmm.covariances[i]=np.cov(mod_dataset.T, aweights=responsibilities[:, i]) # +np.diag(np.zeros((train_constructs["dimensionality"]))+train_constructs["reg_covar"])
+        logging.debug("det of component {} cov: {}".format(i, np.linalg.det(gmm.covariances[i])))
+        gmm.mixing_coeffs[i]=sum(responsibilities[:, i])/mod_dataset.shape[0]
 
-	logging.debug("Iteraion {} of EM algorithm done".format(opt_idx))
+    nlls.append(-np.sum(np.log(prob)))
+
+    logging.debug("Iteration {} of EM algorithm done".format(opt_idx))
+
+logging.debug("Ending EM")
 
 save_gmm(gmm, pathjoin(train_constructs["logging_dir"], "trained_model.gmm"))
-np.savetxt(pathjoin(train_constructs["logging_dir"], "NLL.csv"), nlls, delim=",", header="NEGATIVE_LOG_LIKELIHOOD")
+np.savetxt(pathjoin(train_constructs["logging_dir"], "NLL.csv"), nlls, delimiter=",", header="NEGATIVE_LOG_LIKELIHOOD")
 save_nll_plot()
+
+logging.debug("Generating samples")
+samples=pca.inverse_transform(gmm.sample(50)).reshape((50, 28, 28))
+fig, axs=plt.subplots(5, 10, figsize=(50, 10))
+
+for i in range(50):
+    axs[int(i/10), int(i%10)].imshow(samples[i])
+
+fig.suptitle("50 generated samples")
+fig.savefig(pathjoin(train_constructs["logging_dir"], "generated_samples.jpeg"))
+fig.show()
+
+

trainConstructs.py
@@ -1,15 +1,19 @@
+import pandas as pd
+import numpy as np
 import logging
-import torch
-import torchaudio
+from os.path import join as pathjoin
 
 train_constructs={}
-train_constructs["num_components"]=1000
-train_constructs["dimensionality"]=1
+train_constructs["num_components"]=3
+train_constructs["dimensionality"]=28*28
 train_constructs["num_iters"]=1000
-train_constructs["logging_dir"]="D:\\Vaibhav_Personal\\Entertainment\\song_generation\\run1_RB\\"
-train_constructs["logging_level"]=20
+train_constructs["logging_dir"]=""
+train_constructs["logging_level"]=0
+train_constructs["reg_covar"]=1e-5
 
 
 def getDataset():
-	logging.debug("Loading RB ABUDHABI dataset")
-	return (torchaudio.load("data/RB_ABUDHABI.wav")[0]).numpy().reshape((-1, 1))
+	logging.debug("Loading MNIST train")
+	data=pd.read_csv("data/mnist_train.csv").values[:100, 1:]*50/255
+	print(np.linalg.det(np.cov(data.T)))
+	return data